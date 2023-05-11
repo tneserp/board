ceb9a5a2d4b2795bb4f8e799e39bf0b184564b71
tneserp <tneserp2@gmail.com>
2023-05-11 16:35:54 +0900

    initial commit

---
+++ src/test/java/tneserp/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package tneserp.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
